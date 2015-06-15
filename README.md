70a7cf18471da201f346c76a46b0edd1e32c53db
tate <tate@localhost.localdomain>
2015-06-15 23:58:29 +0900

    Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # go_hash
+ This class is implementation of "Hash" in Ruby or "Dictionary" in Java by using "Map" in golang.
+
+# Goal
+
+
+# Test-framework
+ This is tested by "Gospel", which is Behaviour Driven Development Testing Framework and named after a certain movie.
--- address.go
+++ address.go
@@ -1,5 +1,33 @@
 import "fmt"
 
 type Dictionary interface {
-	
+	insert(int,string)
+	search(int) string
+	delete(int)
+}
+
+type Dict struct{
+	Dict []string
+	Stack []int
+}
+
+func (dict Dict) insert(key int,value string) {
+	dict[key] = value
+}
+
+func (dict Dict) search(key int) {
+	return dict[key]
+}
+
+func (dict Dict) delete(key int) {
+  dict[key] = null
+}
+
+func (dict Dict) init(length int){
+	dict
+}
+
+func main(){
+
+
 }
