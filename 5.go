0ec500a13f2289d8b810ab7ebc6a71890d11e088
tate <tate@localhost.localdomain>
2015-06-25 22:54:34 +0900

    calc impld 5.go

--- 5.go
+++ 5.go
@@ -23,30 +23,32 @@ func main() {
     sc.Split(bufio.ScanWords)
     m := nextInt()
     n := nextInt()
-		q := make([]int,0)
-		r := make([]int,0)
-		sum_q := 0
-		sum_r := 0
+    q := make([]int,0)
+    r := make([]int,0)
+    sum_q := 0
+    sum_r := 0
+    
     for i := 0; i < n; i++ {
         q = append(q,nextInt())
-				sum_q += q[len(q)-1]
+        sum_q += q[len(q)-1]
         r = append(r,nextInt())
-				sum_r += r[len(r)-1]
+        sum_r += r[len(r)-1]
     }
     //fmt.Println(m,q,r,sum_q,sum_r)
 
 		//calculate
-		dp_m := sum_q - m
-		dp := make([]int,dp_m+1)
-		for i:=0; i<n ;i++ {
-			r_tmp := r[i]
-			q_tmp := q[i]
+    dp_m := sum_q - m
+    dp := make([]int,dp_m+1)
+    for i:=0; i<n ;i++ {
+        r_tmp := r[i]
+        q_tmp := q[i]
     //fmt.Println(dp,r[i],q[i])
-			for j:=dp_m; j>= q_tmp; j-- {
-				if dp[j] < dp[j-q_tmp]+r_tmp{
-				dp[j] = dp[j-q_tmp]+r_tmp}
-			}
-		}
-		fmt.Println(sum_r-dp[dp_m])
+        for j:=dp_m; j>= q_tmp; j-- {
+            if dp[j] < dp[j-q_tmp]+r_tmp{
+                dp[j] = dp[j-q_tmp]+r_tmp
+            }
+        }
+    }
+    fmt.Println(sum_r-dp[dp_m])
 }
 
