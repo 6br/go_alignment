22b4a58f28263188430e6b107063c9b489850404
6br <tate@localhost.com>
2015-11-16 20:46:06 +0900

    Use goroutine

--- src/real_linear_gotoh.go
+++ src/real_linear_gotoh.go
@@ -56,8 +56,8 @@ func (l *Rgotoh) Length() {
 		l.h[2][0][j] = l.Cost(j)
 	}
 
-  for j := 1; j <= n; j++ {
-	  for i := 1; i <= m; i++ {
+	for j := 1; j <= n; j++ {
+		for i := 1; i <= m; i++ {
 			var nexth int
 			//Update H^1 (l.h[0])
 			nexth, _ = Max(l.h[0][i-1][0], l.h[1][i-1][0], l.h[2][i-1][0])
@@ -71,15 +71,15 @@ func (l *Rgotoh) Length() {
 			nexth, _ = Max(l.h[0][i][0]-d, math.MinInt64, l.h[2][i][0]-e)
 			l.h[2][i][1] = nexth
 		}
-		for i:=1; i<=m; i++{
-		  l.h[0][i][0] = l.h[0][i][1]
-		  l.h[1][i][0] = l.h[1][i][1]
-		  l.h[2][i][0] = l.h[2][i][1]
+		for i := 1; i <= m; i++ {
+			l.h[0][i][0] = l.h[0][i][1]
+			l.h[1][i][0] = l.h[1][i][1]
+			l.h[2][i][0] = l.h[2][i][1]
 		}
 	}
 }
 
-func (l *Rgotoh) RegionAlign(i1 int, i2 int, j1 int, j2 int) (result int) {
+func (l *Rgotoh) RegionAlign(i1 int, i2 int, j1 int, j2 int) (result int, class int) {
 	var m = i2 - i1
 	var n = j2 - j1
 	l.h[1][0][0] = math.MinInt64
@@ -95,8 +95,8 @@ func (l *Rgotoh) RegionAlign(i1 int, i2 int, j1 int, j2 int) (result int) {
 		l.h[2][0][j] = l.Cost(j)
 	}
 
-  for j := 1; j <= n; j++ {
-	  for i := 1; i <= m; i++ {
+	for j := 1; j <= n; j++ {
+		for i := 1; i <= m; i++ {
 			var nexth int
 			//Update H^1 (l.h[0])
 			nexth, _ = Max(l.h[0][i-1][0], l.h[1][i-1][0], l.h[2][i-1][0])
@@ -110,14 +110,14 @@ func (l *Rgotoh) RegionAlign(i1 int, i2 int, j1 int, j2 int) (result int) {
 			nexth, _ = Max(l.h[0][i][0]-d, math.MinInt64, l.h[2][i][0]-e)
 			l.h[2][i][1] = nexth
 		}
-		for i:=1; i<=m; i++{
-		  l.h[0][i][0] = l.h[0][i][1]
-		  l.h[1][i][0] = l.h[1][i][1]
-		  l.h[2][i][0] = l.h[2][i][1]
+		for i := 1; i <= m; i++ {
+			l.h[0][i][0] = l.h[0][i][1]
+			l.h[1][i][0] = l.h[1][i][1]
+			l.h[2][i][0] = l.h[2][i][1]
 		}
 	}
-	e, _ := Max(l.h[0][m][0], l.h[1][m][0], l.h[2][m][0])
-	return e
+	e, k := Max(l.h[0][m][0], l.h[1][m][0], l.h[2][m][0])
+	return e, k
 }
 
 func (l Rgotoh) ScoreArgs(x int, y int) (int, int) {
@@ -128,101 +128,67 @@ func (l Rgotoh) ScoreArgs(x int, y int) (int, int) {
 func (l Rgotoh) Print(i int, j int) (string, string, string) {
 	//_, arg := l.ScoreArgs(i, j)
 	//fmt.Println(l.RegionAlign(0,i,0,j))
-	return l.LinearSpaceAlign(0,i,0,j)//Print_iter(i, j, arg)
+	return l.LinearSpaceAlign(0, i, 0, j) //Print_iter(i, j, arg)
 }
 
-func (l Rgotoh) LinearSpaceAlign(i1 int, i2 int, j1 int, j2 int) (p string, q string, r string){
-  i := i2-i1-1
-	j := j2-j1-1
+func (l Rgotoh) LinearSpaceAlign(i1 int, i2 int, j1 int, j2 int) (p string, q string, r string) {
+	i := i2 - i1
+	j := j2 - j1
+	fmt.Println(i1, i2, j1, j2)
 	if i <= 0 && j <= 0 {
 		return "", "", ""
 	} else if i <= 0 {
 		//p,q,r = l.Print(i,j-1)
-		for ; j > 0; j-- {
+		for ; j2 > j1; j2-- {
 			p += "-"
 			q += " "
-			r += fmt.Sprintf("%c", l.y[j-1])
+			r += fmt.Sprintf("%c", l.y[j2-1])
 		}
 		return p, q, r
 	} else if j <= 0 {
 		//p,q,r = l.Print(i-1,j)
-		for ; i > 0; i-- {
-			p += fmt.Sprintf("%c", l.x[i-1])
+		for ; i2 > i1; i2-- {
+			p += fmt.Sprintf("%c", l.x[i2-1])
 			q += " "
 			r += "-"
 		}
 		return p, q, r
-	} else if i==1 && j==1{
-    return "a", "b", "c"
+	} else if i == 1 && j == 1 {
+		p += fmt.Sprintf("%c", l.x[i1])
+		q += "|"
+		r += fmt.Sprintf("%c", l.y[i1])
+		return p, q, r
 	}
-  ih := ((i1+i2)/2)
-	if ih >= len(l.x) || ih<i1{panic(i1+i2)}
-  maxscore, maxj := -10000, -100000
-    fmt.Println(i1,i2,j1,j2,ih,maxj)
-	for jh:=j1;jh<j2;jh++{
-	fmt.Println(ih,i2,jh,j2)
-    tmp := l.RegionAlign(i1,ih,j1,jh) + l.RegionAlign(ih,i2,jh,j2)
-		fmt.Println(i1,i2,j1,j2,ih,jh,tmp)
-		if tmp >= maxscore{
-		  maxscore = tmp
-		  maxj = jh
+	ih := ((i1 + i2) / 2)
+	if ih >= len(l.x) || ih < i1 {
+		panic(i1 + i2)
+	}
+	maxscore, maxj := math.MinInt64, -1
+	fmt.Println(i1, i2, j1, j2, ih, maxj)
+	for jh := j1; jh < j2; jh++ {
+		tmp1, err1 := l.RegionAlign(i1, ih, j1, jh)
+		tmp2, err2 := l.RegionAlign(ih, i2, jh, j2)
+		tmp := tmp1 + tmp2
+		_, d := l.Geted()
+		if err1 >= 1 && err2 >= 1 {
+			tmp -= d
+		}
+		fmt.Println(i1, i2, j1, j2, ih, jh, tmp)
+		if tmp >= maxscore {
+			maxscore = tmp
+			maxj = jh
 		}
 	}
 
-		p, q, r = l.LinearSpaceAlign(i1, ih, j1, maxj)
-		p += fmt.Sprintf("%c", l.x[ih])
-		q += "|"
-		r += fmt.Sprintf("%c", l.y[maxj])
-		p2, q2, r2 := l.LinearSpaceAlign(ih, i2, maxj, j2)
-		p += p2
-		q += q2
-		r += r2
+	p, q, r = l.LinearSpaceAlign(i1, ih, j1, maxj)
+	p += fmt.Sprintf("%c", l.x[ih])
+	q += "|"
+	r += fmt.Sprintf("%c", l.y[maxj])
+	p2, q2, r2 := l.LinearSpaceAlign(ih, i2, maxj, j2)
+	p += p2
+	q += q2
+	r += r2
 	return
-}
-
-func (l Rgotoh) Print_iter(i int, j int, arg int) (string, string, string) {
-  return "a","b","c"
-	var p, q, r string
-	if i <= 0 && j <= 0 {
-		return "", "", ""
-	} else if i <= 0 {
-		//p,q,r = l.Print(i,j-1)
-		for ; j > 0; j-- {
-			p += "-"
-			q += " "
-			r += fmt.Sprintf("%c", l.y[j-1])
-		}
-		return p, q, r
-	} else if j <= 0 {
-		//p,q,r = l.Print(i-1,j)
-		for ; i > 0; i-- {
-			p += fmt.Sprintf("%c", l.x[i-1])
-			q += " "
-			r += "-"
-		}
-		return p, q, r
-	}
-	e, d := l.Geted()
-	if arg == 0 {
-		_, arg = l.ScoreArgs(i-1, j-1)
-		p, q, r = l.Print_iter(i-1, j-1, arg)
-		p += fmt.Sprintf("%c", l.x[i-1])
-		q += "|"
-		r += fmt.Sprintf("%c", l.y[j-1])
-	} else if arg == 1 {
-		_, arg = Max(l.h[0][i-1][j]-d, l.h[1][i-1][j]-e, l.h[2][i-1][j]-d)
-		p, q, r = l.Print_iter(i-1, j, arg)
-		p += fmt.Sprintf("%c", l.x[i-1])
-		q += " "
-		r += "-"
-	} else {
-		_, arg = Max(l.h[0][i][j-1]-d, math.MinInt64, l.h[2][i][j-1]-e)
-		p, q, r = l.Print_iter(i, j-1, arg)
-		p += "-"
-		q += " "
-		r += fmt.Sprintf("%c", l.y[j-1])
-	}
-	return p, q, r
 }
 
 /*
