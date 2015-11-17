8bf56b8d1c56714166b1ef987919a995f55911c4
Intern <intern@Naoki-no-MacBook-Pro.local>
2015-11-18 08:35:29 +0900

    Fix Benchmark

--- main_test.go
+++ main_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func Benchmark3(b *testing.B) {
-	//ary, ary2 := readfasta("sequences.txt")
-	ary := readfile("NM_000690.fasta")
-	ary2 := readfile("XM_0062493585.2.fasta")
+	ary, ary2 := readfasta("sequences.txt")
+	//ary := readfile("NM_000690.fasta")
+	//ary2 := readfile("XM_0062493585.2.fasta")
 	settings := readconfig("config.txt")
 	b.ResetTimer()
 	lcs := NewGotoh(ary, ary2, settings)
@@ -18,11 +18,24 @@ func Benchmark3(b *testing.B) {
 }
 
 func Benchmark5(b *testing.B) {
-	ary := readfile("NM_000690.fasta")
-	ary2 := readfile("XM_0062493585.2.fasta")
+	ary, ary2 := readfasta("sequences.txt")
+	//ary := readfile("NM_000690.fasta")
+	//ary2 := readfile("XM_0062493585.2.fasta")
 	settings := readconfig("config.txt")
 	b.ResetTimer()
 	lcs := NewLGotoh(ary, ary2, settings)
+	lcs.Length() // Exec alignment
+	var lx, ly = lcs.Strlen()
+	var _, _, _ = lcs.Print(lx, ly)
+}
+
+func Benchmark6(b *testing.B) {
+	ary, ary2 := readfasta("sequences.txt")
+	//ary := readfile("NM_000690.fasta")
+	//ary2 := readfile("XM_0062493585.2.fasta")
+	settings := readconfig("config.txt")
+	b.ResetTimer()
+	lcs := NewRGotoh(ary, ary2, settings)
 	lcs.Length() // Exec alignment
 	var lx, ly = lcs.Strlen()
 	var _, _, _ = lcs.Print(lx, ly)
